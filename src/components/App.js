157470ea095a08a21d374a17004c3e6ac223f02c
Prabhakaran Ramaswamy <new.prabhakaran@gmail.com>
2016-12-27 09:44:21 +0530

    Adding the ToolBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import MenuBar from './MenuBar/MenuBar';
+import AppToolBar from './AppToolBar/AppToolBar';
 import {Grid, Row, Col} from 'react-flexbox-grid';
 
 
@@ -8,27 +9,14 @@ class App extends React.Component {
   render() {
     return (
       <MuiThemeProvider>
-
-        <Grid>
-        <Row>
-          <Col xs={12}>
-            <Row center="xs">
-              <Col xs={6} />
-            </Row>
-          </Col>
-        </Row>
-
-        <Row>
-        <Col xs={12}>
-          <Row start="xs">
-            <Col xs={6} />
+        <Grid fluid={true}>
+          <Row>
+            <AppToolBar/>
           </Row>
-        </Col>
-        </Row>
+          <Row>
+            <AppToolBar/>
+          </Row>
 
-        <Row>
-          <Col xs={6} md={3}> <MenuBar /> </Col>
-        </Row>
         <Row around="xs">
           <Col xs={2} />
           <Col xs={2} />
